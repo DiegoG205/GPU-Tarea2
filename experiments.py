ce6b2c2f7d37548adcb77eaf978608a34d0da7c5
diego <diego@terminus.cl>
2024-05-12 18:34:19 -0400

    add first experiments

--- experiments.py
+++ experiments.py
@@ -1,17 +1,23 @@
-import os
 import random
+import subprocess
 
 
-n = 64
+n = 8
 s = 1000
 c = 10
 t = 64
 
-for i in range(0,c):
-    seed = random.randint(1000,9999)
-    print(f'Current exp: {i + 1}')
-    os.popen(f'.\\build\\src\\Debug\\MyProjectCPU.exe {n} {s} outcpu.txt {seed}')
-    os.popen(f'.\\build\\src\\cuda\\Debug\\MyProjectCUDA.exe {n} {s} {t} 0 0 outCuda.txt {seed}')
-    os.popen(f'.\\build\\src\\cuda\\Debug\\MyProjectCUDA.exe {n} {s} {t} 1 0 outCudaShared.txt {seed}')
-    os.popen(f'.\\build\\src\\cuda\\Debug\\MyProjectCUDA.exe {n} {s} {t} 0 1 outCuda2D.txt {seed}')
+
+for i in range(0, 4): # 7 iteraciones llega hasta n=1024
+    n *= 2
+    for j in range(0, c):
+        seed = random.randint(1000,9999)
+        p = subprocess.Popen(['.\\build\\src\\Debug\\MyProjectCPU.exe', str(n), str(s), "outSeq.txt", str(seed)])
+        p.wait()
+        p = subprocess.Popen(['.\\build\\src\\cuda\\Debug\\MyProjectCUDA.exe', str(n), str(s), str(t), '0', '0', "outCuda.txt", str(seed)])
+        p.wait()
+        p = subprocess.Popen(['.\\build\\src\\cuda\\Debug\\MyProjectCUDA.exe', str(n), str(s), str(t), '1', '0', "outCudaShared.txt", str(seed)])
+        p.wait()
+        p = subprocess.Popen(['.\\build\\src\\cuda\\Debug\\MyProjectCUDA.exe', str(n), str(s), str(t), '0', '1', "outCuda2D.txt", str(seed)])
+        p.wait()
 
