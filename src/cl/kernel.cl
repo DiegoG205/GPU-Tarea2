f5e66672f39c25ae0717a05033cef3f575c620a6
LeonelEspinoza <leonel.espinoza@ug.uchile.cl>
2024-05-21 02:45:29 -0400

    Update cl main and kernel batch

--- src/cl/kernel.cl
+++ src/cl/kernel.cl
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 kernel void vec_sum(global int *a, global int *b, global int *c, int N) {
   int gindex = get_global_id(0);
   if (gindex < N) {
--- src/cl/main.cpp
+++ src/cl/main.cpp
@@ -9,6 +9,8 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <random>
+
 
 struct Times {
   long create_data;
@@ -54,17 +56,24 @@ bool init() {
 
 /*
 * @param N : 
-* @param localSize : 
-* @param globalSize : 
+* @param Steps : 
+* @param sharedMem : 
+* @param threads2D : 
+* @param seed : 
 *********************/
 
-bool simulate(int N, int Steps, int blocksize, int sharedMem, int threads2D, int seed) {
+bool simulate(int N, int Steps, int blockSize, int sharedMem, int threads2D, int seed) {
   using std::chrono::microseconds;
   
-  if (!seed) std::srand(std::time(0));
-  else std::srand(seed);
+  std::default_random_engine gen;
 
-  std::size_t size = sizeof(cl_double4) * N;
+  if (!seed){ 
+    std::cout << "Random seed\n";
+    gen.seed(std::time(0));
+    //std::srand(std::time(0));
+  }
+  else gen.seed(seed);
+
   std::size_t size = sizeof(cl_double4) * N;
   std::vector<cl_double4> posData(N);
   std::vector<cl_double4> velData(N);
@@ -72,16 +81,22 @@ bool simulate(int N, int Steps, int blocksize, int sharedMem, int threads2D, int
   // Create the memory buffers
   cl::Buffer posDev(queue.getInfo<CL_QUEUE_CONTEXT>(), CL_MEM_READ_WRITE, size);
   cl::Buffer velDev(queue.getInfo<CL_QUEUE_CONTEXT>(), CL_MEM_READ_WRITE, size);
+  cl::Buffer auxPosDev(queue.getInfo<CL_QUEUE_CONTEXT>(), CL_MEM_READ_WRITE, size);
+  cl::Buffer auxVelDev(queue.getInfo<CL_QUEUE_CONTEXT>(), CL_MEM_READ_WRITE, size);
 
   // Assign values to host variables
   auto t_start = std::chrono::high_resolution_clock::now();
+
+  std::uniform_real_distribution<double> pos(0.0, 10000.0);
+  std::uniform_real_distribution<double> mass(50000.0, 100000.0);
+
   for (int i = 0; i < N; i++) {
-    posData[i].x = double(std::rand() % 10000);
-    posData[i].y = double(std::rand() % 10000);
-    posData[i].z = double(std::rand() % 10000);
-    posData[i].w = double(std::rand() % 25000 + 50000);
-    velData[i] = {0,0,0,0};
+    posData[i].x = pos(gen);
+    posData[i].y = pos(gen);
+    posData[i].z = pos(gen);
+    posData[i].w = mass(gen);
   }
+  for (int i = 0; i < N; i++) velData[i] = {0,0,0,0};
   auto t_end = std::chrono::high_resolution_clock::now();
   t.create_data =
       std::chrono::duration_cast<microseconds>(t_end - t_start).count();
@@ -94,60 +109,115 @@ bool simulate(int N, int Steps, int blocksize, int sharedMem, int threads2D, int
   // Copy values from host variables to device
   t_start = std::chrono::high_resolution_clock::now();
   // usar CL_FALSE para hacerlo asíncrono
-  queue.enqueueWriteBuffer(posDev, CL_TRUE, 0, size, a.data());
-  queue.enqueueWriteBuffer(velDev, CL_TRUE, 0, size, b.data());
+  queue.enqueueWriteBuffer(posDev, CL_TRUE, 0, size, posData.data());
+  queue.enqueueWriteBuffer(velDev, CL_TRUE, 0, size, velData.data());
   t_end = std::chrono::high_resolution_clock::now();
   t.copy_to_device =
       std::chrono::duration_cast<microseconds>(t_end - t_start).count();
 
   // Make kernel
   if (threads2D) {
-    const dim3 threads(8, 8, 1);
+    const cl_uint3 threads(8, 8, 1);
     int blocknum = (N + 63)/64;
-	  const dim3 blocks((blocknum+1)/2, 2, 1);
+	  const cl_uint3 blocks((blocknum+1)/2, 2, 1);
     if (sharedMem) {
       std::cerr << "Can't activate both options at the same time\n";
       //nbody_kernel_shared_2D<<<blocks, threads, (sizeof(int4)*64)>>>(N, posDev, velDev, Steps, 8, blocks.x, blocks.y);
     }
     else {
-      //kernel w threads2D and no shared memory
-      nbody_kernel_2D<<<blocks, threads>>>(N, posDev, velDev, Steps, blocks.x * threads.x);
+      while(Steps--){
+        // Make kernel
+        //nbody_kernel_2D<<<blocks, threads>>>(N, posDev, auxPosDev, velDev, auxVelDev, blocks.x * threads.x);
+        cl::Kernel kernel(prog, "nbody_kernel_2D");
+
+        // Set the kernel arguments
+        
+        kernel.setArg(0, N);
+        kernel.setArg(1, posDev);
+        kernel.setArg(2, auxPosDev);
+        kernel.setArg(3, velDev);
+        kernel.setArg(4, auxVelDev);
+        kernel.setArg(5, blocks.x * threads.x);
+
+        // Execute the function on the device (using 32 threads here)
+        cl::NDRange gSize(globalSize);//Cantidad Total WorkItems
+        cl::NDRange lSize(localSize);//WorkItems por WorkGroup
+
+        queue.enqueueNDRangeKernel(kernel, cl::NullRange, gSize, lSize);
+        queue.finish();
+        //cudaDeviceSynchronize!!!
+        queue.enqueueReadBuffer(auxPosDev, CL_TRUE, 0, size, &posDev);
+        queue.enqueueReadBuffer(auxVelDev, CL_TRUE, 0, size, &velDev);
+        //cudaDeviceSynchronize!!!
+      }
     }
   }
   else {
-    int gridSize = (N + blockSize - 1)/blockSize;
+    int gridSize = (N + blockSize -1)/blockSize;
 
-    // Execute the function on the device (using 32 threads here)
+    //Execute the function on the device (using 32 threads here)
     if (sharedMem) {
-      // Shared memory
+      //Shared memory
       std::cout << "Using shared memory: \n";
       t_start = std::chrono::high_resolution_clock::now();
-      nbody_kernel_shared<<<blockSize, gridSize, (sizeof(int4)*blockSize)>>>(N, posDev, velDev, Steps, blockSize, gridSize);
-      cudaDeviceSynchronize();
-    } 
+      while(Steps--){
+        // Make kernel
+        // nbody_kernel_shared<<<blockSize, gridSize, (sizeof(double4)*blockSize)>>>(N, posDev, auxPosDev, velDev, auxVelDev, blockSize, gridSize);
+        cl::Kernel kernel(prog, "nbody_kernel_shared");
+
+        // Set the kernel arguments
+        kernel.setArg(0, N);
+        kernel.setArg(1, posDev);
+        kernel.setArg(2, auxPosDev);
+        kernel.setArg(3, velDev);
+        kernel.setArg(4, auxVelDev);
+        kernel.setArg(5, blockSize);
+        kernel.setArg(6, gridSize);
+
+        // Execute the function on the device (using 32 threads here)
+        cl::NDRange gSize(globalSize);//Cantidad Total WorkItems
+        cl::NDRange lSize(localSize);//WorkItems por WorkGroup
+
+        queue.enqueueNDRangeKernel(kernel, cl::NullRange, gSize, lSize);
+        queue.finish();
+        //cudaDeviceSynchronize!!!
+        queue.enqueueReadBuffer(auxPosDev, CL_TRUE, 0, size, &posDev);
+        queue.enqueueReadBuffer(auxVelDev, CL_TRUE, 0, size, &velDev);
+        //cudaDeviceSynchronize!!!
+      }
+    }
     else {
-      // No shared memory
+      //No shared memory
       std::cout << "Not using shared memory: \n";
       t_start = std::chrono::high_resolution_clock::now();
-      
-      nbody_kernel<<<blockSize, gridSize>>>(N, posDev, velDev, Steps);
-      cudaDeviceSynchronize();
+      while(Steps--){
+        // Make kernel
+        // nbody_kernel<<<blockSize, gridSize>>>(N, posDev, auxPosDev, velDev, auxVelDev);
+        cl::Kernel kernel(prog, "nbody_kernel");
+
+        // Set the kernel arguments
+        kernel.setArg(0, N);
+        kernel.setArg(1, posDev);
+        kernel.setArg(2, auxPosDev);
+        kernel.setArg(3, velDev);
+        kernel.setArg(4, auxVelDev);
+        kernel.setArg(5, blockSize);
+        kernel.setArg(6, gridSize);
+
+        // Execute the function on the device (using 32 threads here)
+        cl::NDRange gSize(globalSize);//Cantidad Total WorkItems
+        cl::NDRange lSize(localSize);//WorkItems por WorkGroup
+
+        queue.enqueueNDRangeKernel(kernel, cl::NullRange, gSize, lSize);
+        queue.finish();
+        //cudaDeviceSynchronize!!!
+        queue.enqueueReadBuffer(auxPosDev, CL_TRUE, 0, size, &posDev);
+        queue.enqueueReadBuffer(auxVelDev, CL_TRUE, 0, size, &velDev);
+        //cudaDeviceSynchronize!!!
+      }
     }
   }
-  cl::Kernel kernel(prog, "vec_sum");
-
-  // Set the kernel arguments
-  kernel.setArg(0, aBuff);
-  kernel.setArg(1, bBuff);
-  kernel.setArg(2, cBuff);
-  kernel.setArg(3, N);
-
-  // Execute the function on the device (using 32 threads here)
-  cl::NDRange gSize(globalSize);
-  cl::NDRange lSize(localSize);
-
-  t_start = std::chrono::high_resolution_clock::now();
-  queue.enqueueNDRangeKernel(kernel, cl::NullRange, globalSize, localSize);
+  
   queue.finish();
   t_end = std::chrono::high_resolution_clock::now();
   t.execution =
@@ -156,7 +226,7 @@ bool simulate(int N, int Steps, int blocksize, int sharedMem, int threads2D, int
 
   // Copy the output variable from device to host
   t_start = std::chrono::high_resolution_clock::now();
-  queue.enqueueReadBuffer(cBuff, CL_TRUE, 0, size, c.data());
+  queue.enqueueReadBuffer(posDev, CL_TRUE, 0, size, posData.data());
   t_end = std::chrono::high_resolution_clock::now();
   t.copy_to_host =
       std::chrono::duration_cast<std::chrono::microseconds>(t_end - t_start)
@@ -165,8 +235,7 @@ bool simulate(int N, int Steps, int blocksize, int sharedMem, int threads2D, int
   // Print the result
   std::cout << "RESULTS: " << std::endl;
   for (int i = 0; i < N; i++)
-    std::cout << "  out[" << i << "]: " << c[i] << " (" << a[i] << " + " << b[i]
-              << ")\n";
+        std::cout << " Particula " << i << ": (" << posData[i].x << ", " << posData[i].y << ", " << posData[i].z << ")\n";
 
   std::cout << "Time to create data: " << t.create_data << " microseconds\n";
   std::cout << "Time to copy data to device: " << t.copy_to_device
@@ -176,10 +245,11 @@ bool simulate(int N, int Steps, int blocksize, int sharedMem, int threads2D, int
             << " microseconds\n";
   std::cout << "Time to execute the whole program: " << t.total()
             << " microseconds\n";
+
   return true;
 }
 
-bool simulate(int N, int localSize, int globalSize) {
+bool simulate_default(int N, int localSize, int globalSize) {
   using std::chrono::microseconds;
   std::size_t size = sizeof(int) * N;
   std::vector<int> a(N), b(N), c(N);
